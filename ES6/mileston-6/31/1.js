2cb033a2c8c82a905d4637ca254b5eb0d5a6c93a
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-08-24 15:49:39 +0600

    786

---
+++ ES6/mileston-6/31/1.js
@@ -0,0 +1,26 @@
+// 1 array 
+const numbers = [1, 2, 3, 4, 6];
+const numberCount = numbers.length; // Count total pice of array elamint
+numbers.pop(); // remove single pice of array elamint
+numbers.push(); // add single pice of array elamint
+numbers[2] = 555; // change 3 pocition of array elamint
+
+// 2 check whether 222 included in the array
+if (numbers.includes(222) != -1) {}
+if (numbers.includes(222)) {}
+
+// 3 loop = while, for 
+for (const number of numbers) {}
+
+// 4 function
+function fullName(first, second) {
+    const name = first + '' + second;
+}
+const person = fullName('Tanvir', 'Ali');
+
+// 5 object
+const bottle = {
+    color: 'yellow',
+    contain: 'water',
+    price: 50,
+}
---
+++ ES6/mileston-6/31/2-dom.js
@@ -0,0 +1,5 @@
+document.getElementById('add-border').addEventListener('click', function() {
+    // console.log('add border clicked'); // for check add border button work
+    const container = document.getElementById('friend-container');
+    container.style.border = '3px solid yellow';
+});
