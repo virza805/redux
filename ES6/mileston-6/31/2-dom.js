c85a0a2611f61820eae09e254a2635b46c730999
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-08-24 16:28:27 +0600

    add dom

--- ES6/mileston-6/31/2-dom.js
+++ ES6/mileston-6/31/2-dom.js
@@ -3,3 +3,23 @@ document.getElementById('add-border').addEventListener('click', function() {
     const container = document.getElementById('friend-container');
     container.style.border = '3px solid yellow';
 });
+
+// add background color
+document.getElementById('add-bgColor').addEventListener('click', function() {
+    const friends = document.getElementsByClassName('friend');
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'lightblue';
+    }
+});
+
+// add Friend
+document.getElementById('add-friend').addEventListener('click', function() {
+    const container = document.getElementById('friend-container');
+
+    container.innerHTML = `
+    <div class="friend">
+        <h3 class="friend-name">Tanzil</h3>
+        <p>He is student</p>
+    </div>
+    `;
+});
