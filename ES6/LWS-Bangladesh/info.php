5077fbbda8a9cd7a76bfa6e7e429801cb78cde9d
MD AL AMIN <01mdalamin1@gmail.com>
2022-09-16 17:05:07 +0600

    add link

--- ES6/LWS-Bangladesh/info.php
+++ ES6/LWS-Bangladesh/info.php
@@ -4,6 +4,8 @@
 // https://github.com/learnwithsumit/think-in-a-react-way/tree/lesson-3
 
 /**
+ * https://www.youtube.com/watch?v=-oGMwOOHFyk&list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr&index=5
+ * https://github.com/learnwithsumit/think-in-a-react-way/tree/lesson-4
  * 
  * callback & promise
  * 
