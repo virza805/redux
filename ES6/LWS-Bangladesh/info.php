e64e4d45a34fb133f618d08918079c327e7e72e1
MD AL AMIN <01mdalamin1@gmail.com>
2022-09-17 12:51:21 +0600

    react Rademat folder

--- ES6/LWS-Bangladesh/info.php
+++ ES6/LWS-Bangladesh/info.php
@@ -1,8 +1,5 @@
 <?php 
 
-// https://www.youtube.com/watch?v=ii8GaRjRoNI&list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr&index=3
-// https://github.com/learnwithsumit/think-in-a-react-way/tree/lesson-3
-
 /**
  * https://www.youtube.com/watch?v=-oGMwOOHFyk&list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr&index=5
  * https://github.com/learnwithsumit/think-in-a-react-way/tree/lesson-4
